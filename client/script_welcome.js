58c3fc6fb10875e23cdb05dd451500cc8254c95f
SWARAJ-42 <marslegends2004@gmail.com>
2023-04-07 12:11:21 +0530

    OOPed the code

--- client/script_welcome.js
+++ client/script_welcome.js
@@ -1,50 +1,103 @@
 window.addEventListener('load', function () {
     const canvas = document.getElementById('canvas1');
-    const textInput = document.getElementById('textInput');
     const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
     const text = "JARVIS";
     console.log(ctx);
-    ctx.fillStyle = 'aqua';
-    ctx.strokeStyle = "blue";
 
-    ctx.font = "80px Helvetica"
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
+    class Particle {
+        constructor() {
 
-    const maxTextwidth = canvas.width * .8;
-    const lineHeight = 80;
-
-    function wrapText(text) {
-        let linesArray = [];
-        let linesCounter = 0;
-        let line = "";
-        let words = text.split(" ");
-        for (let i = 0; i < words.length; i++) {
-            let testLine = line + words[i] + " ";
-            if (ctx.measureText(testLine).width > maxTextwidth) {
-                line = words[i] + " ";
-                linesCounter++;
-            } else {
-                line = testLine;
-            }
-            linesArray[linesCounter] = line;
         }
-        let textHeight = lineHeight * linesCounter;
-        let textY = canvas.height / 2 - textHeight / 2;
+        draw() {
 
-        linesArray.forEach((el, index) => {
-            ctx.fillText(el, canvas.width / 2, textY + index * lineHeight);
-            ctx.strokeText(el, canvas.width / 2, textY + index * lineHeight);
-        });
-        // console.log(linesArray);
+        }
+        update() {
+
+        }
+
     }
-    wrapText(text);
 
-    textInput.addEventListener('keyup', function (e) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        wrapText(e.target.value);
-    })
+    class Effect {
+        constructor(context, canvasWidth, canvasHeight) {
+            this.context = context;
+            this.canvasWidth = canvasWidth;
+            this.canvasHeight = canvasHeight;
+            this.textX = this.canvasWidth / 2;
+            this.textY = this.canvasHeight / 2;
+            this.fontsize = 100;
+            this.lineHeight = this.fontsize * .8;
+            this.maxTextwidth = this.canvasWidth * .8;
+            this.textInput = document.getElementById('textInput');
+            this.textInput.addEventListener('keyup',(e) => {
+                if (e.key !== " ") {
+                    this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+                    this.wrapText(e.target.value);
+                }
+            });
+            // particles text
+            this.particles = [];
+            this.gap = 3;
+            this.mouse = {
+                radius: 20000,
+                x: 0,
+                y: 0
+            }
+            window.addEventListener('mousemove', (e) => {
+                this.mouse.x = e.x;
+                this.mouse.y = e.y;
+            })
+        }
+        wrapText(text) {
+            const gradient = this.context.createLinearGradient(0, 0, this.canvasWidth, this.canvasHeight);
+            gradient.addColorStop(.3, 'red');
+            gradient.addColorStop(.5, 'fuchsia');
+            gradient.addColorStop(.7, 'purple');
+            this.context.fillStyle = gradient;
+            this.context.lineWidth = 3;
+            this.context.strokeStyle = "aqua";
+            this.context.font = this.fontsize + "px Helvetica"
+            this.context.textAlign = 'center';
+            this.context.textBaseline = 'middle';
+
+            // Arrangeing Text
+            let linesArray = [];
+            let linesCounter = 0;
+            let line = "";
+            let words = text.split(" ");
+            for (let i = 0; i < words.length; i++) {
+                let testLine = line + words[i] + " ";
+                if (this.context.measureText(testLine).width > this.maxTextwidth) {
+                    line = words[i] + " ";
+                    linesCounter++;
+                } else {
+                    line = testLine;
+                }
+                linesArray[linesCounter] = line;
+            }
+            let textHeight = this.lineHeight * linesCounter;
+            this.textY = this.canvasHeight / 2 - textHeight / 2;
+
+            linesArray.forEach((el, index) => {
+                this.context.fillText(el.trim(), this.canvasWidth / 2, this.textY + index * this.lineHeight);
+                this.context.strokeText(el.trim(), this.canvasWidth / 2, this.textY + index * this.lineHeight);
+            });
+        }
+        convertToPartricles() {
+
+        }
+        render() {
+
+        }
+    }
+
+    const effect = new Effect(ctx, canvas.width, canvas.height);
+    effect.wrapText(text);
+    console.log(effect);
+
+    function animate() {
+
+    }
 });
