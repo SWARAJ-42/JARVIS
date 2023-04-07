af44cb8294f7b48f5dfec4a900f97933097dba8f
SWARAJ-42 <marslegends2004@gmail.com>
2023-04-07 16:20:06 +0530

    Text decoration completed

--- client/script_welcome.js
+++ client/script_welcome.js
@@ -1,21 +1,49 @@
 window.addEventListener('load', function () {
     const canvas = document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext('2d', {
+        willReadFrequently: true
+    });
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
     const text = "JARVIS";
-    console.log(ctx);
 
     class Particle {
-        constructor() {
-
+        constructor(effect, x, y, color) {
+            this.effect = effect;
+            this.x = Math.random() * this.effect.canvasWidth;
+            this.y = Math.random() * this.effect.canvasHeight;
+            this.color = color;
+            this.originX = x;
+            this.originY = y;
+            this.size = this.effect.gap - 1;
+            this.dx = 0;
+            this.dy = 0;
+            this.vx = 0;
+            this.vy = 0;
+            this.angle = 0;
+            this.distance = 0;
+            this.friction = Math.random() * 0.6 + .15;
+            this.ease = Math.random() * .05 + .005;
         }
         draw() {
-
+            this.effect.context.fillStyle = this.color;
+            this.effect.context.fillRect(this.x, this.y, this.size, this.size);
         }
         update() {
+            this.dx = this.effect.mouse.x - this.x;
+            this.dy = this.effect.mouse.y - this.y;
+            this.distance = this.dx * this.dx + this.dy * this.dy;
+            this.force = -this.effect.mouse.radius / this.distance;
 
+            if (this.distance < this.effect.mouse.radius) {
+                this.angle = Math.atan2(this.dy, this.dx);
+                this.vx = this.force * Math.cos(this.angle);
+                this.vy = this.force * Math.sin(this.angle);
+            }
+
+            this.x += (this.vx *= this.friction) + (this.originX - this.x) * this.ease;
+            this.y += (this.vy *= this.friction) + (this.originY - this.y) * this.ease;
         }
 
     }
@@ -27,21 +55,22 @@ window.addEventListener('load', function () {
             this.canvasHeight = canvasHeight;
             this.textX = this.canvasWidth / 2;
             this.textY = this.canvasHeight / 2;
-            this.fontsize = 100;
+            this.fontsize = this.canvasWidth / 12;
             this.lineHeight = this.fontsize * .8;
             this.maxTextwidth = this.canvasWidth * .8;
-            this.textInput = document.getElementById('textInput');
-            this.textInput.addEventListener('keyup',(e) => {
-                if (e.key !== " ") {
-                    this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-                    this.wrapText(e.target.value);
-                }
-            });
+            this.verticalOffset = 0;
+            // this.textInput = document.getElementById('textInput');
+            // this.textInput.addEventListener('keyup', (e) => {
+            //     if (e.key !== " ") {
+            //         this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+            //         this.wrapText(e.target.value);
+            //     }
+            // });
             // particles text
             this.particles = [];
             this.gap = 3;
             this.mouse = {
-                radius: 20000,
+                radius: this.fontsize * 100,
                 x: 0,
                 y: 0
             }
@@ -55,10 +84,10 @@ window.addEventListener('load', function () {
             gradient.addColorStop(.3, 'red');
             gradient.addColorStop(.5, 'fuchsia');
             gradient.addColorStop(.7, 'purple');
-            this.context.fillStyle = gradient;
+            this.context.fillStyle = "aqua";
             this.context.lineWidth = 3;
             this.context.strokeStyle = "aqua";
-            this.context.font = this.fontsize + "px Helvetica"
+            this.context.font = this.fontsize + "px Orbitron"
             this.context.textAlign = 'center';
             this.context.textBaseline = 'middle';
 
@@ -84,20 +113,43 @@ window.addEventListener('load', function () {
                 this.context.fillText(el.trim(), this.canvasWidth / 2, this.textY + index * this.lineHeight);
                 this.context.strokeText(el.trim(), this.canvasWidth / 2, this.textY + index * this.lineHeight);
             });
+            this.convertToPartricles();
         }
         convertToPartricles() {
-
+            this.particles = [];
+            const pixels = this.context.getImageData(0, 0, this.canvasWidth, this.canvasHeight).data;
+            this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+            for (let y = 0; y < this.canvasHeight; y += this.gap) {
+                for (let x = 0; x < this.canvasWidth; x += this.gap) {
+                    const index = (y * this.canvasWidth + x) * 4;
+                    const alpha = pixels[index + 3];
+                    if (alpha > 0) {
+                        const red = pixels[index];
+                        const green = pixels[index + 1];
+                        const blue = pixels[index + 2];
+                        const color = `rgb('${red}', '${green}', '${blue}')`;
+                        this.particles.push(new Particle(this, x, y, color));
+                    }
+                }
+            }
         }
         render() {
-
+            this.particles.forEach(particle => {
+                particle.update();
+                particle.draw();
+            });
         }
     }
 
     const effect = new Effect(ctx, canvas.width, canvas.height);
     effect.wrapText(text);
-    console.log(effect);
+    effect.render();
 
     function animate() {
-
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        effect.render();
+        requestAnimationFrame(animate);
     }
+
+    animate();
 });
