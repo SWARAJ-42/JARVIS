d80e35079c4af49b7a80d56d8b2b01b72754b619
SWARAJ-42 <biswalswaraj88@kgpian.iitkgp.ac.in>
2024-12-27 11:41:42 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -1,15 +1,13 @@
 import express from "express";
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from 'openai';
 
 dotenv.config();
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-})
-
-const openai = new OpenAIApi(configuration);
+const client = new OpenAI({
+    apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+});
 
 const app = express();
 app.use(cors());
@@ -24,18 +22,12 @@ app.get('/', async (req, res) => {
 app.post('/', async (req, res) => {
     try {
         const prompt = req.body.prompt;
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
-            temperature: 0,
-            max_tokens: 3000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
-        });
-
+        const response = await client.chat.completions.create({
+            messages: [{ role: 'user', content: prompt }],
+            model: 'gpt-4o',
+          });
         res.status(200).send({
-            bot: response.data.choices[0].text
+            bot: response.choices[0].message.content
         })
     } catch (error) {
         console.log(error);
